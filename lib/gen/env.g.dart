61214b58d6894644920200a45c7c0d21fdf63ad8
Imon Ghosh <48136286+ImonGhosh@users.noreply.github.com>
2025-11-27 16:18:12 +0530

    Generate env.g.dart with local environment values

---
+++ lib/gen/env.g.dart
@@ -0,0 +1,8 @@
+// GENERATED WITH LOCAL VALUES FOR BUILD PIPELINES
+// Do not edit manually; update .env and regenerate if needed.
+
+class Env {
+  static const apiUrl = 'https://welcome-app.farafin.de/api/public';
+  static const apiDevUrl = 'http://10.0.2.2:8080/api/public';
+  static const jwtSecret = '123456';
+}
